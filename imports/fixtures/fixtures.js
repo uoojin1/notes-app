c2df78aadf170b810ed4cfb1c997eb911342ec8e
Eugene Moon <uoojin95@gmail.com>
2017-12-02 17:21:33 -0500

    added another teest case for noteListItem.test.js

--- imports/fixtures/fixtures.js
+++ imports/fixtures/fixtures.js
@@ -3,13 +3,13 @@ export const notes = [
         _id: 'noteId1',
         title: 'Test title',
         body: '',
-        updatedAt: 0,
+        updatedAt: 123121231,
         userId: 'userId1'
     }, {
         _id: 'noteId2',
         title: '',
         body: 'something is inside the body',
-        updatedAt: 0,
+        updatedAt: 123121231,
         userId: 'userId2'
     }
 ];
