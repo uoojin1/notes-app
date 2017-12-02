9286b57ed5e61f98e838f3a62b048be4b8cf32f6
Eugene Moon <uoojin95@gmail.com>
2017-12-02 17:10:33 -0500

    added fixtures folder

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const notes = [
+    {
+        _id: 'noteId1',
+        title: 'Test title',
+        body: '',
+        updatedAt: 0,
+        userId: 'userId1'
+    }, {
+        _id: 'noteId2',
+        title: '',
+        body: 'something is inside the body',
+        updatedAt: 0,
+        userId: 'userId2'
+    }
+];
--- imports/ui/NoteList.test.js
+++ imports/ui/NoteList.test.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import expect from 'expect';
-import {mount} from 'enzyme';
-import {Meteor} from 'meteor/meteor';
+import { mount } from 'enzyme';
+import { Meteor } from 'meteor/meteor';
 
-import {NoteList} from './NoteList';
-
-const notes = [
-    {
-        _id: 'noteId1',
-        title: 'Test title',
-        body: '',
-        updatedAt: 0,
-        userId: 'userId1'
-    }, {
-        _id: 'noteId2',
-        title: '',
-        body: 'something is inside the body',
-        updatedAt: 0,
-        userId: 'userId2'
-    }
-];
+import { notes } from '../fixtures/fixtures';
+import { NoteList } from './NoteList';
 
 if(Meteor.isClient){
     describe('NoteList', function() {
