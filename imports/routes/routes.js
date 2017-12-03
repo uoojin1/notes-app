f5983d3abb4f0d1fe13ee0d6d7c99817c01b3a6b
Eugene Moon <uoojin95@gmail.com>
2017-12-02 19:49:46 -0500

    refactor router to allow for dynamic urls

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -10,43 +10,38 @@ import Login from '../ui/Login';
 
 window.browserHistory = browserHistory;
 
-const unauthenticatedPages = ['/', '/signup'];
-const authenticatedPages = ['/dashboard'];
-
-const onEnterPublicPage = () => {
-  if(Meteor.userId()) {
-    browserHistory.replace('/dashboard');
-  }
-};
-
-const onEnterPrivatePage = () => {
-  if(!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
-
 const onEnterNotePage = (nextState) => {
-  if(!Meteor.userId()) {
-    browserHistory.replace('/');
-  } else {
-    Session.set('selectedNoteId', nextState.params.id);
-  }
+  Session.set('selectedNoteId', nextState.params.id);
+};
+
+const onLeaveNotePage = () => {
+  Session.set('selectedNoteId', undefined);
+};
+
+export const globalOnChange = (prevState, nextState) => {
+  globalOnEnter(nextState);
+};
+
+export const globalOnEnter = (nextState) => {
+  const lastRoute = nextState.routes[nextState.routes.length-1];
+  Session.set('currentPagePrivacy', lastRoute.privacy);
 };
 
 export const routes = (
   <Router history={browserHistory}>
-    <Route path='/' component={Login} onEnter={onEnterPublicPage}/>
-    <Route path='/signup' component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path='/dashboard' component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path='/dashboard/:id' component={Dashboard} onEnter={onEnterNotePage}/>
-    <Route path='/*' component={NotFound}/>
+    <Route onEnter={globalOnEnter} onChange={globalOnChange}>
+      <Route path='/' component={Login} privacy="unauth"/>
+      <Route path='/signup' component={Signup} privacy="unauth"/>
+      <Route path='/dashboard' component={Dashboard} privacy="auth"/>
+      <Route path='/dashboard/:id' component={Dashboard} privacy="auth" onEnter={onEnterNotePage} onLeave={onLeaveNotePage}/>
+      <Route path='/*' component={NotFound}/>
+    </Route>
   </Router>
 );
 
-export const onAuthChange = (isAuthenticated) => {
-    const pathname = browserHistory.getCurrentLocation().pathname;
-    const isUnAuthenticatedPage = unauthenticatedPages.includes(pathname); // true or false
-    const isAuthenticatedPage = authenticatedPages.includes(pathname);
+export const onAuthChange = (isAuthenticated, currentPagePrivacy) => {
+    const isUnAuthenticatedPage = currentPagePrivacy === 'unauth';
+    const isAuthenticatedPage = currentPagePrivacy === 'auth';
   
     if(isUnAuthenticatedPage && isAuthenticated) {
       browserHistory.replace('/dashboard');
