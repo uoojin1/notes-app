bad5d9437b498f8c5f83d2848fa71562955e4f5b
Eugene Moon <uoojin95@gmail.com>
2017-12-03 21:37:35 -0500

    added styling for the editor 2

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -43,15 +43,17 @@ export class Editor extends React.Component {
         if(this.props.note) {
             return (
                 <div className="editor">
-                    <input value={this.state.title} placeholder="note title" onChange={this.handleTitleChange.bind(this)}/>
-                    <textarea value={this.state.body} placeholder="Write note here" onChange={this.handleBodyChange.bind(this)}></textarea>
-                    <button onClick={this.handleRemoval.bind(this)}>Delete Note</button>
+                    <input className="editor__title" value={this.state.title} placeholder="note title" onChange={this.handleTitleChange.bind(this)}/>
+                    <textarea className="editor__body" value={this.state.body} placeholder="Write note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+                    <div>
+                        <button className="button button--secondary" onClick={this.handleRemoval.bind(this)}>Delete Note</button>
+                    </div> 
                 </div>             
             );
         } else { 
             return (
                 <div className="editor">
-                    <p>{ this.props.selectedNoteId ? 'Note not found' : 'Pick or create a note'}</p>
+                    <p className="editor__message">{ this.props.selectedNoteId ? 'Note not found' : 'Pick or create a note'}</p>
                 </div>
             );
         }
